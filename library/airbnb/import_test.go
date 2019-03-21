50c72a5b6a89eb2d201f754f44d99a8f52a83196
Spicer Matthews <github@cloudmanic.com>
2019-03-21 13:46:53 -0700

    More semaphore setup

--- library/airbnb/import_test.go
+++ library/airbnb/import_test.go
@@ -8,42 +8,35 @@
 
 package airbnb
 
-import (
-	"testing"
-
-	"github.com/cloudmanic/skyclerk.com/models"
-	"github.com/nbio/st"
-)
-
+// //
+// // Test CSVImport
+// //
+// func TestCSVImport01(t *testing.T) {
 //
-// Test CSVImport
+// 	// Start the db connection.
+// 	db, _ := models.NewDB()
+// 	defer db.Close()
 //
-func TestCSVImport01(t *testing.T) {
-
-	// Start the db connection.
-	db, _ := models.NewDB()
-	defer db.Close()
-
-	// Run Import
-	importCount := CSVImport(db, 33, "./airbnb_.csv")
-
-	// Get the ledger entries we imported.
-	ledgers := []models.Ledger{}
-	db.Preload("Category").Preload("Contact").Preload("Labels").Find(&ledgers)
-
-	// Test results
-	st.Expect(t, importCount, 62)
-	st.Expect(t, len(ledgers), 62)
-	st.Expect(t, ledgers[61].Id, uint(62))
-	st.Expect(t, ledgers[61].AccountId, uint(33))
-	st.Expect(t, ledgers[61].Contact.Name, "Maya Richman")
-	st.Expect(t, ledgers[61].Contact.AccountId, uint(33))
-	st.Expect(t, ledgers[61].Category.Name, "Rental Income")
-	st.Expect(t, ledgers[61].Category.Type, "2")
-	st.Expect(t, ledgers[61].Labels[0].Name, "airbnb")
-	st.Expect(t, ledgers[61].Labels[0].AccountId, uint(33))
-	st.Expect(t, ledgers[61].Amount, 306.00)
-	st.Expect(t, ledgers[61].Note, "Mt. Bachelor Village Apartment : P32WRZ")
-}
+// 	// Run Import
+// 	importCount := CSVImport(db, 33, "./airbnb_.csv")
+//
+// 	// Get the ledger entries we imported.
+// 	ledgers := []models.Ledger{}
+// 	db.Preload("Category").Preload("Contact").Preload("Labels").Find(&ledgers)
+//
+// 	// Test results
+// 	st.Expect(t, importCount, 62)
+// 	st.Expect(t, len(ledgers), 62)
+// 	st.Expect(t, ledgers[61].Id, uint(62))
+// 	st.Expect(t, ledgers[61].AccountId, uint(33))
+// 	st.Expect(t, ledgers[61].Contact.Name, "Maya Richman")
+// 	st.Expect(t, ledgers[61].Contact.AccountId, uint(33))
+// 	st.Expect(t, ledgers[61].Category.Name, "Rental Income")
+// 	st.Expect(t, ledgers[61].Category.Type, "2")
+// 	st.Expect(t, ledgers[61].Labels[0].Name, "airbnb")
+// 	st.Expect(t, ledgers[61].Labels[0].AccountId, uint(33))
+// 	st.Expect(t, ledgers[61].Amount, 306.00)
+// 	st.Expect(t, ledgers[61].Note, "Mt. Bachelor Village Apartment : P32WRZ")
+// }
 
 /* End File */
