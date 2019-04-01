e125edfa2a73dcfa65c1e15b8fe544f8a8b40eec
Spicer Matthews <github@cloudmanic.com>
2019-04-01 07:58:41 -0700

    Cleaned up a bunch of things with deploy

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Date: 10/10/2017
+# Author(s): Spicer Matthews (spicer@options.cafe)
+# Copyright: 2017 Cloudmanic Labs, LLC. All rights reserved.
+#
+# Deploy the entire app (frontend and backend). We do some compiling locally and then deploy.
+
+# cd to backend
+cd ..
+
+# Build backend
+echo "Building app.skyclerk.com"
+env GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o builds/app.skyclerk.com
+upx builds/app.skyclerk.com
+
+cd scripts
+
+# Deploy to backend with Ansible
+cd ../ansible
+ansible-playbook app.yml
+cd ../scripts
