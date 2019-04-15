4877e53ba920ef9bae3f9f5b97023815b55fd0e7
Spicer Matthews <github@cloudmanic.com>
2019-04-14 17:29:29 -0700

    Refacted middleware

--- backend/controllers/middlewares.go
+++ backend/controllers/middlewares.go
@@ -19,6 +19,7 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/cloudmanic/skyclerk.com/backend/library/realip"
+	"github.com/cloudmanic/skyclerk.com/backend/models"
 	"github.com/cloudmanic/skyclerk.com/backend/services"
 )
 
@@ -27,63 +28,14 @@ import (
 //
 func (t *Controller) AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Set access token and start the auth process
-		var access_token = ""
+		// Validate and Set the user.
+		user := t.AuthUser(c)
 
-		// Make sure we have a Bearer token.
-		auth := strings.SplitN(c.Request.Header.Get("Authorization"), " ", 2)
-
-		if len(auth) != 2 || auth[0] != "Bearer" {
-
-			// We allow access token from the command line
-			if os.Getenv("APP_ENV") == "local" {
-
-				access_token = c.Query("access_token")
-
-				if len(access_token) <= 0 {
-					c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization Failed (#001)"})
-					c.AbortWithStatus(401)
-					return
-				}
-
-			} else {
-				c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization Failed (#002)"})
-				c.AbortWithStatus(401)
-				return
-			}
-
-		} else {
-			access_token = auth[1]
-		}
-
-		// See if this session is in our db.
-		session, err := t.db.GetByAccessToken(access_token)
-
-		if err != nil {
-			services.LogInfo("Access Token Not Found - Unable to Authenticate via HTTP (#003)")
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization Failed (#003)"})
-			c.AbortWithStatus(401)
+		if user.Id <= 0 {
 			return
 		}
 
-		// Get this user is in our db.
-		user, err := t.db.GetUserById(session.UserId)
-
-		if err != nil {
-			services.LogInfo("User Not Found - Unable to Authenticate - UserId (HTTP) : " + fmt.Sprint(session.UserId) + " - Session Id : " + fmt.Sprint(session.Id))
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization Failed (#004)"})
-			c.AbortWithStatus(401)
-			return
-		}
-
-		// Log this request into the last_activity col.
-		session.LastActivity = time.Now()
-		session.LastIpAddress = realip.RealIP(c.Request)
-		t.db.New().Save(&session)
-
-		// Add this user to the context
-		c.Set("userId", user.Id)
-
+		// Get the account
 		accountId, err := strconv.ParseInt(c.Param("account"), 10, 32)
 
 		if err != nil {
@@ -110,10 +62,8 @@ func (t *Controller) AuthMiddleware() gin.HandlerFunc {
 		// Set Account
 		c.Set("accountId", int(accountId))
 
-		// CORS for local deve opment.
-		if os.Getenv("APP_ENV") == "local" {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		}
+		// Set the CORS header
+		t.SetCors(c)
 
 		// On to next request in the Middleware chain.
 		c.Next()
@@ -125,70 +75,89 @@ func (t *Controller) AuthMiddleware() gin.HandlerFunc {
 //
 func (t *Controller) AuthNoAccountMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Set access token and start the auth process
-		var access_token = ""
+		// Validate and Set the user.
+		t.AuthUser(c)
 
-		// Make sure we have a Bearer token.
-		auth := strings.SplitN(c.Request.Header.Get("Authorization"), " ", 2)
-
-		if len(auth) != 2 || auth[0] != "Bearer" {
-
-			// We allow access token from the command line
-			if os.Getenv("APP_ENV") == "local" {
-
-				access_token = c.Query("access_token")
-
-				if len(access_token) <= 0 {
-					c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization Failed (#001)"})
-					c.AbortWithStatus(401)
-					return
-				}
-
-			} else {
-				c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization Failed (#002)"})
-				c.AbortWithStatus(401)
-				return
-			}
-
-		} else {
-			access_token = auth[1]
-		}
-
-		// See if this session is in our db.
-		session, err := t.db.GetByAccessToken(access_token)
-
-		if err != nil {
-			services.LogInfo("Access Token Not Found - Unable to Authenticate via HTTP (#003)")
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization Failed (#003)"})
-			c.AbortWithStatus(401)
-			return
-		}
-
-		// Get this user is in our db.
-		user, err := t.db.GetUserById(session.UserId)
-
-		if err != nil {
-			services.LogInfo("User Not Found - Unable to Authenticate - UserId (HTTP) : " + fmt.Sprint(session.UserId) + " - Session Id : " + fmt.Sprint(session.Id))
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization Failed (#004)"})
-			c.AbortWithStatus(401)
-			return
-		}
-
-		// Log this request into the last_activity col.
-		session.LastActivity = time.Now()
-		session.LastIpAddress = realip.RealIP(c.Request)
-		t.db.New().Save(&session)
-
-		// Add this user to the context
-		c.Set("userId", user.Id)
-
-		// CORS for local deve opment.
-		if os.Getenv("APP_ENV") == "local" {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		}
+		// Set the CORS header
+		t.SetCors(c)
 
 		// On to next request in the Middleware chain.
 		c.Next()
+	}
+}
+
+//
+// AuthUser the user part of the requst.
+//
+func (t *Controller) AuthUser(c *gin.Context) models.User {
+	// Set access token and start the auth process
+	var access_token = ""
+
+	// Make sure we have a Bearer token.
+	auth := strings.SplitN(c.Request.Header.Get("Authorization"), " ", 2)
+
+	if len(auth) != 2 || auth[0] != "Bearer" {
+
+		// We allow access token from the command line
+		if os.Getenv("APP_ENV") == "local" {
+
+			access_token = c.Query("access_token")
+
+			if len(access_token) <= 0 {
+				c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization Failed (#001)"})
+				c.AbortWithStatus(401)
+				return models.User{}
+			}
+
+		} else {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization Failed (#002)"})
+			c.AbortWithStatus(401)
+			return models.User{}
+		}
+
+	} else {
+		access_token = auth[1]
+	}
+
+	// See if this session is in our db.
+	session, err := t.db.GetByAccessToken(access_token)
+
+	if err != nil {
+		services.LogInfo("Access Token Not Found - Unable to Authenticate via HTTP (#003)")
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization Failed (#003)"})
+		c.AbortWithStatus(401)
+		return models.User{}
+	}
+
+	// Get this user is in our db.
+	user, err := t.db.GetUserById(session.UserId)
+
+	if err != nil {
+		services.LogInfo("User Not Found - Unable to Authenticate - UserId (HTTP) : " + fmt.Sprint(session.UserId) + " - Session Id : " + fmt.Sprint(session.Id))
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization Failed (#004)"})
+		c.AbortWithStatus(401)
+		return models.User{}
+	}
+
+	// Log this request into the last_activity col.
+	session.LastActivity = time.Now()
+	session.LastIpAddress = realip.RealIP(c.Request)
+	t.db.New().Save(&session)
+
+	// Add this user to the context
+	c.Set("userId", user.Id)
+
+	// Return happy
+	return user
+}
+
+//
+// SetCors header
+//
+func (t *Controller) SetCors(c *gin.Context) {
+	// CORS for local deve opment.
+	if os.Getenv("APP_ENV") == "local" {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	}
 }
 
