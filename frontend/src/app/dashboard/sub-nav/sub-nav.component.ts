85f585d7a2dd9fefe676467e04b8985aca15ca26
Spicer Matthews <github@cloudmanic.com>
2019-07-06 10:45:38 -0700

    Made subnav work for active class

--- frontend/src/app/dashboard/sub-nav/sub-nav.component.ts
+++ frontend/src/app/dashboard/sub-nav/sub-nav.component.ts
@@ -1,10 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+//
+// Date: 2019-06-26
+// Author: Spicer Matthews (spicer@skyclerk.com)
+// Last Modified by: Spicer Matthews
+// Copyright: 2019 Cloudmanic Labs, LLC. All rights reserved.
+//
+
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
 	selector: 'app-dashboard-sub-nav',
 	templateUrl: './sub-nav.component.html'
 })
+
 export class SubNavComponent implements OnInit {
+	@Input() current: string = "";
 
 	constructor() { }
 
@@ -12,3 +21,5 @@ export class SubNavComponent implements OnInit {
 	}
 
 }
+
+/* End File */
