7d4de1dc4624158c499a3e4cc41eab2062eac736
Spicer Matthews <github@cloudmanic.com>
2019-09-21 13:37:24 -0700

    Got update account owner working.

--- frontend/src/app/settings/account/account-owner/account-owner.component.ts
+++ frontend/src/app/settings/account/account-owner/account-owner.component.ts
@@ -6,6 +6,13 @@
 //
 
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user.model';
+import { MeService } from 'src/app/services/me.service';
+import { Me } from 'src/app/models/me.model';
+import { AccountService } from 'src/app/services/account.service';
+import { Account } from 'src/app/models/account.model';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-settings-account-account-owner',
@@ -13,18 +20,92 @@ import { Component } from '@angular/core';
 })
 
 export class AccountOwnerComponent {
+	me: Me = new Me();
 	editMode: boolean = false;
+	users: User[] = [];
+	account: Account = new Account();
+	userName: string = "";
 
 	//
 	// Constructor
 	//
-	constructor() { }
+	constructor(public meService: MeService, public userService: UserService, public accountService: AccountService, public router: Router) { }
+
+	//
+	// ngOnInit
+	//
+	ngOnInit() {
+		// Get Me.
+		this.meService.get().subscribe(res => {
+			this.me = res;
+		});
+
+		// Get the active account.
+		this.accountService.getAccount().subscribe(res => {
+			this.account = res;
+			this.setDisplayName();
+		});
+
+		// Get the users.
+		this.loadUsers();
+	}
+
+	//
+	// Load users
+	//
+	loadUsers() {
+		this.userService.get().subscribe((res) => {
+			this.users = res;
+			this.setDisplayName();
+		});
+	}
+
+	//
+	// Set display name
+	//
+	setDisplayName() {
+		for (let i = 0; i < this.users.length; i++) {
+			if (this.account.OwnerId == this.users[i].Id) {
+				this.userName = this.users[i].FirstName + " " + this.users[i].LastName;
+				return;
+			}
+		}
+	}
+
+	//
+	// Update the user title in the selector.
+	//
+	onChange() {
+		console.log(this.account.OwnerId);
+		this.setDisplayName();
+	}
 
 	//
 	// Toggle to edit mode.
 	//
 	editModeToggle() {
 		this.editMode = !this.editMode;
+	}
+
+	//
+	// On save.
+	//
+	onSave() {
+		let c = confirm("Are you sure you want to change the owner? You will not be able to access account or billing settings after this.");
+
+		if (!c) {
+			return;
+		}
+
+		// Close widget.
+		this.editModeToggle();
+
+		// Send updated account to server.
+		this.accountService.update(this.account).subscribe(() => {
+			// Reset the current account.
+			this.accountService.setActiveAccount();
+			this.router.navigate(['/']);
+		});
 	}
 }
 
--- frontend/src/app/settings/sub-nav/sub-nav.component.ts
+++ frontend/src/app/settings/sub-nav/sub-nav.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Account } from 'src/app/models/account.model';
+import { MeService } from 'src/app/services/me.service';
+import { AccountService } from 'src/app/services/account.service';
+import { Me } from 'src/app/models/me.model';
 
 @Component({
 	selector: 'app-settings-sub-nav',
@@ -7,9 +11,42 @@ import { Component, OnInit, Input } from '@angular/core';
 export class SubNavComponent implements OnInit {
 	@Input() current: string = "";
 
-	constructor() { }
+	me: Me = new Me();
+	account: Account = new Account();
+	isOwner: boolean = false;
 
+	//
+	// Construct.
+	//
+	constructor(public meService: MeService, public accountService: AccountService) { }
+
+	//
+	// ngOnInit
+	//
 	ngOnInit() {
+		// Get Me.
+		this.meService.get().subscribe(res => {
+			this.me = res;
+			this.setOwner();
+		});
+
+		// Get the active account.
+		this.accountService.getAccount().subscribe(res => {
+			this.account = res;
+			this.setOwner();
+		});
 	}
 
+	//
+	// See if we are an owner
+	//
+	setOwner() {
+		if (this.me.Id == this.account.OwnerId) {
+			this.isOwner = true;
+		} else {
+			this.isOwner = false;
+		}
+	}
 }
+
+/* End File */
