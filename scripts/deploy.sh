3e8d9a1bca5b3eeb3c11c52c017d779068219d32
Spicer Matthews <github@cloudmanic.com>
2023-02-01 15:44:41 -0800

    General clean up for new sever

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,23 +12,25 @@ cd ../backend
 # Build backend
 echo "########## Building app.skyclerk.com ##########"
 env GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o builds/app.skyclerk.com
-upx builds/app.skyclerk.com
+# upx builds/app.skyclerk.com
 cd ../scripts
 
 # Build frontend
 cd ../frontend
 echo "########## Building Frontend ##########"
-ng build --prod
+npm install
+npm run ng build --prod
 cd ../scripts
 
 # Build centcom
 cd ../centcom
 echo "########## Building Centcom ##########"
+npm install
 cd src
 export NODE_ENV=production
 npx tailwindcss build tailwind.css -o styles.css
 cd ..
-ng build --prod --base-href="/centcom/"
+npm run ng build --prod --base-href="/centcom/"
 cd ../scripts
 
 # Deploy to backend with Ansible
