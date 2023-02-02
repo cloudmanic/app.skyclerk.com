f06849d0626471b04b51320b4922ff1b4faf7945
Spicer Matthews <github@cloudmanic.com>
2023-02-02 10:28:31 -0800

    Moved over to a cheaper server.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,23 +15,25 @@ env GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o builds/app.skyclerk.com
 # upx builds/app.skyclerk.com
 cd ../scripts
 
-# Build frontend
-cd ../frontend
-echo "########## Building Frontend ##########"
-npm install
-npm run ng build --prod
-cd ../scripts
+# # Build frontend
+# cd ../frontend
+# echo "########## Building Frontend ##########"
+# #nvm use 12.22.2
+# npm install
+# npm run ng build --prod
+# cd ../scripts
 
-# Build centcom
-cd ../centcom
-echo "########## Building Centcom ##########"
-npm install
-cd src
-export NODE_ENV=production
-npx tailwindcss build tailwind.css -o styles.css
-cd ..
-npm run ng build --prod --base-href="/centcom/"
-cd ../scripts
+# # Build centcom
+# cd ../centcom
+# echo "########## Building Centcom ##########"
+# #nvm use 12.22.2
+# npm install
+# cd src
+# export NODE_ENV=production
+# npx tailwindcss build tailwind.css -o styles.css
+# cd ..
+# npm run ng build --prod --base-href="/centcom/"
+# cd ../scripts
 
 # Deploy to backend with Ansible
 cd ../ansible
